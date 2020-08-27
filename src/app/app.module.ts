5ffc274df8d9d0266193aa95f5cb5adb6e8be752
Misha Djak <23djak@gmail.com>
2020-08-27 18:03:38 +0300

    Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import {HeroesComponent} from './components/heroes/heroes.component';
 import {FormsModule} from '@angular/forms';
 
 import {HeroesStorageService} from './services/heroes-storage/heroes-storage.service';
-import { HeroDatailComponent } from './components/hero-datail/hero-datail.component';
+import {HeroDatailComponent} from './components/hero-datail/hero-datail.component';
 import {MessangerService} from './services/messanger/messanger.service';
-import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
+import {MessagesComponent} from './components/messages/messages.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDatailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
