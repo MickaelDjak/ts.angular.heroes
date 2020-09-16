6051c6ad8f9bfc64a0e594bbb31ecea1bf47478b
MickaelDjak <23djak@gmail.com>
2020-09-16 10:24:11 +0300

    Add Input

--- src/app/conduit/shered/components/pagination/pagination.component.ts
+++ src/app/conduit/shered/components/pagination/pagination.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'cn-sr-pagination',
   templateUrl: './pagination.component.html',
 })
 export class PaginationComponent implements OnInit {
+
+  @Input() baseUrl: string;
+  @Input() total: number;
+  @Input() perPage: number;
+  @Input() currentPage: number;
 
   constructor() { }
 
